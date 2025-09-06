c5729d86b1dd3a24ac5f034ab3eeac08858c17c9
chkrishnaa <chkrishnaa@github.com>
2025-09-06 21:06:07 +0530

    New Modification

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import JobifyFavicon from "./assets/Jobify.png";
+
+const link =
+  document.querySelector("link[rel~='icon']") || document.createElement("link");
+link.rel = "icon";
+link.href = JobifyFavicon;
+document.getElementsByTagName("head")[0].appendChild(link);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
